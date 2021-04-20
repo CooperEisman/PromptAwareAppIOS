bfb040ad81f28bce442df27bc12b46aa6ff858db
CoooperEisman <cooper@eisman.me>
2021-04-19 20:23:39 -0600

    Configuration of CoreData stack and Quick Elementing

--- PromptAware/ContentView.swift
+++ PromptAware/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     var body: some View {
         List {
             ForEach(tasks) { task in
-                Text("Task at \(Task.dateAdded!, formatter: itemFormatter)")
+                Text("Task at \(task.dateAdded!, formatter: itemFormatter)")
             }
             .onDelete(perform: deleteItems)
         }
